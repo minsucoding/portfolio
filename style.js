413a471d6654e9403484ec5afac1bcff615c66d2
minsucoding <79847230+minsucoding@users.noreply.github.com>
2021-05-12 11:17:56 +0900

    Revert "20.04.30"
    This reverts commit 3b2205cfe41438c988b71acae4a212bd971f2ef0.

--- style.js
+++ style.js
@@ -2,10 +2,7 @@ const more_BTN = document.getElementsByClassName('more'),
     animeJS_box = document.querySelector('.animeJS_box'),
     header = document.querySelector('.header'),
     footer = document.querySelector('.footer'),
-    threeD_Box = document.querySelector('.threeD_Box'),
-    screenFront = document.getElementsByClassName('screenFront'),
-    screenMid = document.getElementsByClassName('screenMid'),
-    screenBack = document.getElementsByClassName('screenBack');
+    threeD_Box = document.querySelector('.threeD_Box');
 
 
 
@@ -15,35 +12,3 @@ const more_BTN = document.getElementsByClassName('more'),
     more_BTN[1].addEventListener( 'click', () => {
         threeD_Box.classList.toggle('flex');
     })
-
-    screenFront[0].addEventListener('click', () => {
-        for( z=0; z<screenMid.length; z++){
-            screenMid[0].classList.toggle('rotate');
-        }    
-    })
-    screenBack[0].addEventListener('click', () => {
-        for( z=0; z<screenMid.length; z++){
-            screenMid[0].classList.toggle('rotate');
-        }    
-    })
-    screenFront[1].addEventListener('click', () => {
-        for( z=0; z<screenMid.length; z++){
-            screenMid[1].classList.toggle('rotate');
-        }    
-    })
-    screenBack[1].addEventListener('click', () => {
-        for( z=0; z<screenMid.length; z++){
-            screenMid[1].classList.toggle('rotate');
-        }    
-    })
-    screenFront[2].addEventListener('click', () => {
-        for( z=0; z<screenMid.length; z++){
-            screenMid[2].classList.toggle('rotate');
-        }    
-    })
-    screenBack[2].addEventListener('click', () => {
-        for( z=0; z<screenMid.length; z++){
-            screenMid[2].classList.toggle('rotate');
-        }    
-    })
-    
